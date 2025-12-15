27ff3d43aea3d40cd6e03da24f2500e84101d9e9
Zameer Basha <zameerbasha241@gmail.com>
2025-12-15 23:22:44 +0530

    updated interface

---
+++ src/test/java/nathanda/com/Code/query/CodeQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package nathanda.com.Code.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
